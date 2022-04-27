0e56f768dcfae729a436711c4cbe0cd7939632e8
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-27 11:21:45 +0200

    Nueva lógica imágenes

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -4,7 +4,7 @@ import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
-export function initChart(iframe) {
+export function initChart() {
 
     //Iframe
     setFixedIframeUrl('informe_perfil_mayores_2022_economia_3_8','ejemplo_umbral_pobreza');
@@ -22,5 +22,5 @@ export function initChart(iframe) {
     });
 
     //Altura del frame
-    setChartHeight(iframe);
+    setChartHeight();
 }
